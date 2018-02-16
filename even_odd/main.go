b95bd286d2151b415f716233f92773d0fac9bd36
Brent Gawryluik <brentwg@zoho.com>
2018-02-16 11:51:00 -0800

    Added even_odd assignment

---
+++ even_odd/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	// create a slice of ints 0 - 10
+	myNumbers := []int{0,
+		1,
+		2,
+		3,
+		4,
+		5,
+		6,
+		7,
+		8,
+		9,
+		10}
+
+	// iterate through the slice, check to see if a number is even/odd
+	for number := range myNumbers {
+		if number%2 == 0 {
+			fmt.Println(number, " is even")
+		} else {
+			fmt.Println(number, " is odd")
+		}
+	}
+}
